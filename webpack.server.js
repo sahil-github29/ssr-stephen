2f8ef013bda7c1e84ab8faea7d1976f696918871
sahil-github29 <sahil242927@gmail.com>
2020-05-30 11:27:52 +0530

    used webpack-node-externals package

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.base");
+const webpackNodeExternals = require("webpack-node-externals");
 
 const config = {
   // inform webpack that we are building a bundle for a nodeJS rather than for the browser
@@ -14,6 +15,10 @@ const config = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
   },
+
+  // this rule tells webpack to not to bundle any library in the
+  // output file if the library is inside the node_module folder
+  externals: [webpackNodeExternals()],
 };
 
 module.exports = merge(baseConfig, config);
