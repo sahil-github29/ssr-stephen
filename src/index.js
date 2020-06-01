9438821cbf5ee92c4291a442e160108804e96559
sahil-github29 <sahil242927@gmail.com>
2020-06-01 10:50:48 +0530

    working on react router on server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import "babel-polyfill";
 import express from "express";
 import renderer from "./helpers/renderer";
+import createStore from "./helpers/createStore";
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.get("*", (req, res, next) => {
-  res.send(renderer(req)); // passing req object for <StaticRouter>
+  const store = createStore();
+  res.send(renderer(req, store)); // passing req object for <StaticRouter>
 });
 
 app.listen(3000, () => {
